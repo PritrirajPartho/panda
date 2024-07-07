fa661fe53a6d361f01ab32ab00b77731e865a8e8
Pritiraj Partho <dpartho5616@gmail.com>
2024-07-07 12:35:19 +0600

    navbar start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          300: '#fdba74',
+          500: '#FB923C',
+          800: '#FA760A',
+        },
+        black: '#020617',
+        white: '#fafafa',
+    },
+    screens: {
+      'mobile': '400px',
+      // => @media (min-width: 400px) { ... }
+      'tablet': '768px',
+      // => @media (min-width: 768px) { ... }
+      'desktop': '992px',
+      // => @media (min-width: 992px) { ... }
+    },
   },
   plugins: [],
+}
 }
